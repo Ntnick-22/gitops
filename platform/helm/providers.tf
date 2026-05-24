b5f590ffaa2d72104b8984c7e4da98326534f734
Ntnick-22 <nyeinthunaing322@gmail.com>
2026-05-25 00:27:45 +0100

    version change for helm

--- platform/helm/providers.tf
+++ platform/helm/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 3.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -22,7 +22,7 @@ terraform {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     config_path = "~/.kube/config"
   }
 }
